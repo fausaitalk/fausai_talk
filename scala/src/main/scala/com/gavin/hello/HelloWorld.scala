a6a21a397749e982bee32a643f9eb7c3fecd062a
Gavin <gavin_ke@126.net>
2019-07-02 19:24:08 +0800

    变量

---
+++ scala/src/main/scala/com/gavin/hello/HelloWorld.scala
@@ -0,0 +1,11 @@
+package com.gavin.hello
+
+/**
+  * @Author: Gavin
+  * @Date: 2019/6/26 11:13
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("hello world!")
+  }
+}
---
+++ scala/src/main/scala/com/gavin/talk01/Demo02.scala
@@ -0,0 +1,11 @@
+package com.gavin.talk01
+
+/**
+  * @Author: Gavin
+  * @Date: 2019/7/2 11:28
+  */
+object Demo02 {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
