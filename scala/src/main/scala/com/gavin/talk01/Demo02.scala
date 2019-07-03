d004e66f4431001b7cb2b20236a493e531fed84a
Gavin <gavin_ke@126.net>
2019-07-03 20:53:50 +0800

    变量

--- scala/src/main/scala/com/gavin/talk01/Demo02.scala
+++ scala/src/main/scala/com/gavin/talk01/Demo02.scala
@@ -7,5 +7,14 @@ package com.gavin.talk01
 object Demo02 {
   def main(args: Array[String]): Unit = {
 
+    val num1 = 666 // 默认num1为Int类型
+    val num2 = 18L // 加上L指定num2为Long类型
+
+    val intMax = Int.MaxValue
+    val MinValue = Int.MinValue
+
+    println(s"Int类型最大值为：$intMax，最小值为：$MinValue")
+
+    //val num3 = 21474836470 // 超出了Int范围，报错
   }
 }
---
+++ scala/src/main/scala/com/gavin/talk01/Demo03.scala
@@ -0,0 +1,18 @@
+package com.gavin.talk01
+
+/**
+  * @Author: Gavin
+  * @Date: 2019/7/3 18:25
+  */
+object Demo03 {
+
+  def main(args: Array[String]): Unit = {
+    val num1 = 3.14
+    val f1: Float = 1.1f  //  double->float 错误
+    //  var f2 = 1.2 //  ok 类型推断
+    //  var f3 : Double = 1.3 //  ok
+    //  var f4 : Float = 1.4f //
+    //  okvar f5 : Double = 1.5f  // float->double , ok
+
+  }
+}
